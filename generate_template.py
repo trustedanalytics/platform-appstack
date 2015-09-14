57bba61eb7fdbff53019da5be42a35d7f62ba500
Konrad Reiter <konrad.reiter@intel.com>
2015-09-14 09:29:48 +0200

    DPNG-2267 add facilities for testers team
    make all path absolute generating from current file path or by adding an argument 'directory'

--- generate_template.py
+++ generate_template.py
@@ -1,15 +1,26 @@
 #!/usr/bin/env python
 
 from jinja2 import Environment, FileSystemLoader
-from os.path import dirname, abspath
+from os.path import dirname, abspath, join
 import yaml
+import argparse
 
-THIS_DIR = dirname(abspath(__file__))
-J2_ENV = Environment(loader=FileSystemLoader(THIS_DIR))
+parser = argparse.ArgumentParser()
+parser.add_argument("-d",
+                    "--directory",
+                    default=None,
+                    help="directory where this file is")
+args, unknown = parser.parse_known_args()
 
-with open('template_variables.yml') as f:
-  template_variables = yaml.load(f)
+if args.directory:
+    directory = args.directory
+else:
+    directory = dirname(abspath(__file__))
+J2_ENV = Environment(loader=FileSystemLoader(directory))
+
+with open(join(directory, 'template_variables.yml')) as f:
+    template_variables = yaml.load(f)
 
 rendered_template = J2_ENV.get_template('settings.yml.j2').render(template_variables)
-with open('settings.yml', 'w') as f:
-  f.write(rendered_template)
+with open(join(directory, 'settings.yml'), 'w') as f:
+    f.write(rendered_template)
